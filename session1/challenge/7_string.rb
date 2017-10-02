abbb2018e35d0e724341ef09aa706d272eaa33de
cristhiandas <cristhiandf94@gmail.com>
2017-10-02 13:27:06 +0000

    6th and 7th challenges completed

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,19 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  answer = ""
+  string.size.times do |index|
+    if string[index] == "r" && string[index+1] != nil or string[index] == "R" && string[index+1] != nil
+      answer << string[index + 1]
+    end
+  end
+  answer
 end
+
+pirates_say_arrrrrrrrr("are you really learning Ruby?")
+pirates_say_arrrrrrrrr("Katy Perry is on the radio!")
+pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")
